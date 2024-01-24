b6048a7cbf21dab3f56420ccabc0ce3e6fbb0e7d
vljn <121915880+vljn@users.noreply.github.com>
2024-01-24 04:58:00 +0100

    Rename App to Home

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
+import Home from './Home';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <App />,
+      element: <Home />,
     },
   ]);
 
