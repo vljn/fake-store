3e4c4fc0022f28ab3b9c5c98b3e11202db6a6594
vljn <121915880+vljn@users.noreply.github.com>
2024-01-24 23:59:26 +0100

    Make style changes to the logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,13 +2,15 @@ import logoImage from '../assets/logoImage.svg';
 
 export default function Logo() {
   return (
-    <div className="group flex items-center px-10 py-10 gap-4 cursor-pointer">
+    <div className="group flex items-center gap-4 cursor-pointer">
       <img
         src={logoImage}
-        className="group-hover:-translate-y-3 group-hover:rotate-12 transition-transform transition-duration"
+        className="group-hover:-translate-y-3 group-hover:rotate-12 group-hover:scale-110 transition-transform transition-duration"
         alt=""
       />
-      <h1 className="font-poppins font-bold text-3xl select-none">FakeShop.</h1>
+      <h1 className="font-poppins font-bold text-3xl select-none dark:text-foreground-dark text-foreground-light">
+        FakeShop.
+      </h1>
     </div>
   );
 }
