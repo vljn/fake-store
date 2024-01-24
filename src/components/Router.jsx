a26f314729cb055cf497f08a60fb6c31458d9d39
vljn <121915880+vljn@users.noreply.github.com>
2024-01-24 04:52:25 +0100

    Setup a basic router

---
+++ src/components/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
