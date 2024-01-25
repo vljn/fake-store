ebe9d3375f5d634568c3bbf78a09f5a7e42704a4
vljn <121915880+vljn@users.noreply.github.com>
2024-01-25 03:03:12 +0100

    Add the 404 page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+
+export default function NotFound() {
+  return (
+    <>
+      <Header path={'/'} />
+      <div>
+        <h1 className="dark:text-primary-dark text-primary-light text-9xl font-bold text-center mt-20">
+          404 :/
+        </h1>
+        <p className="text-center dark:text-foreground-dark text-foreground-light mt-14 text-4xl mx-8">
+          sorry, but the page you were looking for was not found.
+        </p>
+      </div>
+    </>
+  );
+}
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Home';
+import NotFound from './NotFound';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Home />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
     },
   ]);
 
