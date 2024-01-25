0f44a6c9534fd31ac2e636eac44f12b512bdff33
vljn <121915880+vljn@users.noreply.github.com>
2024-01-25 03:28:48 +0100

    Style the text when selected

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Button({ children }) {
   return (
-    <button className=" bg-primary-light text-4xl text-primary-content px-6 py-3 rounded-2xl border-primary-dark border-4 dark:hover:border-primary-content transition-[border] hover:border-foreground-light max-sm:text-base max-sm:px-4 max-sm:py-2">
+    <button className=" bg-primary-light text-4xl text-primary-content px-6 py-3 rounded-2xl border-primary-dark border-4 dark:hover:border-primary-content transition-[border] hover:border-foreground-light max-sm:text-base max-sm:px-4 max-sm:py-2 selection:bg-foreground-light">
       {children}
     </button>
   );
