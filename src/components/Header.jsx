1842a66d5381709303b8ebd2971b4bda1426adc6
vljn <121915880+vljn@users.noreply.github.com>
2024-01-25 02:58:30 +0100

    Allow for the logo to have a link to another page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import Logo from './Logo';
 import ThemeSwitcher from './ThemeSwitcher';
+import PropTypes from 'prop-types';
 
-export default function Header() {
+export default function Header({ path }) {
   return (
     <div className="flex items-center justify-between px-10 py-10">
-      <Logo />
+      <Logo path={path} />
       <div>
         <ThemeSwitcher />
       </div>
     </div>
   );
 }
+
+Header.propTypes = {
+  path: PropTypes.string,
+};
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,24 @@
 import logoImage from '../assets/logoImage.svg';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function Logo() {
-  return (
+export default function Logo({ path }) {
+  return path ? (
+    <Link to={path}>
+      <div className="group flex items-center gap-4 cursor-pointer">
+        <img
+          src={logoImage}
+          className="group-hover:-translate-y-3 group-hover:rotate-12 group-hover:scale-110 transition-transform transition-duration"
+          alt=""
+        />
+        <div className="relative">
+          <h1 className="max-sm:text-2xl font-poppins font-bold text-3xl select-none dark:text-foreground-dark text-foreground-light after:content-[' '] after:w-full after:scale-x-0 group-hover:after:scale-x-100 after:h-1 dark:after:bg-foreground-dark after:absolute after:-bottom-2 after:left-0 after:transition-transform after:origin-right after:duration-300">
+            FakeShop.
+          </h1>
+        </div>
+      </div>
+    </Link>
+  ) : (
     <div className="group flex items-center gap-4 cursor-pointer">
       <img
         src={logoImage}
@@ -16,3 +33,7 @@ export default function Logo() {
     </div>
   );
 }
+
+Logo.propTypes = {
+  path: PropTypes.string,
+};
