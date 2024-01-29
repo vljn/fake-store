1f7a3ff12ec728319ca9daca00839b17511c6208
vljn <121915880+vljn@users.noreply.github.com>
2024-01-29 01:02:33 +0100

    Add the app component instead of using the Router component directly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './components/Router';
 import './index.css';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
