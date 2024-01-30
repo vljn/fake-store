59337ee5dcd4c5f55d00a1a4a6f1ea8228cff6de
vljn <121915880+vljn@users.noreply.github.com>
2024-01-31 00:54:37 +0100

    Disable text wrapping for the emoji in the Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function Error({ message }) {
   return (
     <div className="p-6 dark:border-border-dark border-border-light border-2 rounded-xl m-10 mt-5">
-      <h1 className="text-lg md:text-4xl font-bold after:content-[':('] after:pl-6">
+      <h1 className="text-lg md:text-4xl font-bold after:content-[':('] after:pl-6 after:text-nowrap">
         An error has occured
       </h1>
       <p className="italic mt-4">{message}</p>
