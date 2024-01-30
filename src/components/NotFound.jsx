9ae06996a41d2d37c03aab685dd1aa9a483ee016
vljn <121915880+vljn@users.noreply.github.com>
2024-01-31 00:57:50 +0100

    Make the NotFound component more responsive

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,10 +5,10 @@ export default function NotFound() {
     <>
       <Header path={'/'} />
       <div>
-        <h1 className="dark:text-primary-dark text-primary-light text-9xl font-bold text-center mt-20">
+        <h1 className="dark:text-primary-dark text-primary-light text-6xl sm:text-8xl lg:text-9xl font-bold text-center mt-10 sm:mt-20">
           404 :/
         </h1>
-        <p className="text-center mt-14 text-4xl mx-8">
+        <p className="text-center mt-8 sm:mt-10 md:mt-14 sm:text-4xl sm:mx-8 mx-16 text-2xl">
           sorry, but the page you were looking for was not found.
         </p>
       </div>
