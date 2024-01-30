2193470176a49ee1ecb0d3f4a6dfdc4ccbc9d02f
vljn <121915880+vljn@users.noreply.github.com>
2024-01-31 00:29:31 +0100

    Add animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <>
       <Header />
       <div>
-        <h1 className="animate__animated animate__fadeInDow text-center text-6xl font-poppins mt-20 leading-relaxed max-sm:text-3xl">
+        <h1 className="animate__animated animate__fadeInDown text-center text-6xl font-poppins mt-20 leading-relaxed max-sm:text-3xl">
           pay less, buy more.
         </h1>
         <div className="flex justify-center mt-10">
