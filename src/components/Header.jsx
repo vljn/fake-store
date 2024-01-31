422ec700f1a7e873ec91b6f9c651060463e71c29
vljn <121915880+vljn@users.noreply.github.com>
2024-01-31 01:25:44 +0100

    Make the cart and the theme buttons dissapear when scrolling on smaller screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,40 @@ import Logo from './Logo';
 import ThemeSwitcher from './ThemeSwitcher';
 import CartButton from './CartButton';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import 'animate.css';
 
 export default function Header({ path }) {
+  const [lastScroll, setLastScroll] = useState(0);
+  const [showButtons, setShowButtons] = useState(true);
+  let buttonClasses;
+
+  if (showButtons) {
+    buttonClasses = 'animate__animated animate__fadeInDown animate__faster';
+  } else {
+    buttonClasses = 'animate__animated animate__fadeOutUp animate__faster';
+  }
+
+  useEffect(() => {
+    const controlButtonShow = () => {
+      if (window.innerWidth < 640) {
+        if (window.scrollY > lastScroll) {
+          console.log(window.scroll());
+          setShowButtons(false);
+        } else {
+          setShowButtons(true);
+        }
+      }
+      setLastScroll(window.scrollY);
+    };
+
+    window.addEventListener('scroll', controlButtonShow);
+
+    return () => {
+      window.removeEventListener('scroll', controlButtonShow);
+    };
+  }, [lastScroll]);
+
   return (
     <div className="flex items-center justify-between px-10 py-10 max-sm:justify-center">
       <div className="flex items-center gap-16">
@@ -15,7 +47,9 @@ export default function Header({ path }) {
           </h2>
         </Link>
       </div>
-      <div className="max-sm:fixed max-sm:bottom-8 max-sm:right-6 z-10 flex gap-6">
+      <div
+        className={'max-sm:fixed max-sm:bottom-8 max-sm:right-6 z-10 flex gap-6 ' + buttonClasses}
+      >
         <CartButton />
         <ThemeSwitcher />
       </div>
